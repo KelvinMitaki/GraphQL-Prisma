bc80d8d2038e85d00a62fccc1a40b64c4ba3299d
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-10 20:36:36 +0300

    added fetching email for current logged in users only

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,5 +1,21 @@
 import { Context, users } from "../db";
+import getUserId from "../utils/getUserId";
 
-const User = {};
+const User = {
+  async email(parent: any, args: any, ctx: Context, info: any) {
+    const userId = getUserId(ctx.request, false);
+    if (parent.id && userId && parent.id === userId) {
+      return parent.email;
+    }
+    if (userId && !parent.id) {
+      const user = await ctx.prisma.query.user({ where: { id: userId } });
+      if (user.email === parent.email) {
+        return parent.email;
+      }
+      return null;
+    }
+    return null;
+  }
+};
 
 export default User;
