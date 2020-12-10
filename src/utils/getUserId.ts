79783790af5cb35dfd185be54393135930fae2a6
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-10 17:06:18 +0300

    modified getuserid

--- src/utils/getUserId.ts
+++ src/utils/getUserId.ts
@@ -1,16 +1,18 @@
 import { ContextParameters } from "graphql-yoga/dist/types";
 import jwt from "jsonwebtoken";
 
-const getUserId = (req: ContextParameters) => {
+const getUserId = (req: ContextParameters, requireAuth: boolean = true) => {
   const authHeader = req.request.headers.authorization;
-  if (!authHeader) {
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    const verified = jwt.verify(token, "jkshkjhdskjhdskewyoiuoiqw") as {
+      id: string;
+    };
+    return verified.id;
+  }
+  if (requireAuth) {
     throw new Error("auth required");
   }
-  const token = authHeader.split(" ")[1];
-  const verified = jwt.verify(token, "jkshkjhdskjhdskewyoiuoiqw") as {
-    id: string;
-  };
-  return verified.id;
 };
 
 export default getUserId;
